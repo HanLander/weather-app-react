13bfb0bce37fefa728c72d1a69344e74752a27cf
HanLander <157143716+HanLander@users.noreply.github.com>
2024-04-15 17:58:26 +0100

    Added footer with URL's

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="www.linkedin.com/in/hannah-lander-b02311b0">Hannah Lander</a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/HanLander/react-weather-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
